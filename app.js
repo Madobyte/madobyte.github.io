9d182a6b9cb99bd9bf521acffc265e2724baa663
madobyte <marcbambi@gmail.com>
2021-02-16 18:15:38 +0800

    Deleted some unnecessary files.

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 import { createCertificateCards } from './assets/js/createCertificateCards.js';
 import { createProjectCards } from './assets/js/createProjectCards.js';
 import { headerAnimation } from './assets/js/header.js';
-import { hoverProjectCards } from './assets/js/hoverProjectCards.js';
 import { infiniteSlider } from './assets/js/infiniteSlider.js';
-import { scrollAction } from './assets/js/scrollAction.js';
 import { showHideResume } from './assets/js/showHideResume.js'
+import { typeWriter } from './assets/js/typeWriter.js';
 
 headerAnimation();
-scrollAction();
+typeWriter();
 createProjectCards();
-hoverProjectCards();
 showHideResume();
 createCertificateCards();
 infiniteSlider();
