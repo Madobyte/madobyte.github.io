4d99ea6abd896787b852ad71d122bab350732e62
madobyte <marcbambi@gmail.com>
2021-02-19 16:48:11 +0800

    Set up the minified resume.

--- assets/js/showHideResume.js
+++ assets/js/showHideResume.js
@@ -3,7 +3,7 @@ import {createModal} from './createModal.js'
 export const showHideResume = () => {
   const showHideBtn = document.querySelector('.show-hide-btn');
   const content = document.createElement('embed');
-  content.src = './assets/pdf/Engr_Marc_Bambalan_Associate_Software_Engineer.pdf';
+  content.src = './assets/pdf/Engr_Marc_Bambalan_Software_Developer.pdf';
   content.setAttribute('pluginspage', 'http://www.adobe.com/products/acrobat/readstep2.html');
   
   showHideBtn.addEventListener('click', () => createModal(content))
