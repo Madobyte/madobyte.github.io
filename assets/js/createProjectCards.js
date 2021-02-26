0565046ca01717355a3f6d1a411160e7a0b45560
madobyte <marcbambi@gmail.com>
2021-02-26 19:55:15 +0800

    Added the weather app to project list

--- assets/js/createProjectCards.js
+++ assets/js/createProjectCards.js
@@ -1,17 +1,17 @@
-import { projects } from "./projects.js"
+import { projects } from "./projects.js";
 
-export const  createProjectCards = () => {
-  projects.forEach(project => {
-    const projectsContainer = document.getElementById('projects-container');
+export const createProjectCards = () => {
+  projects.forEach((project) => {
+    const projectsContainer = document.getElementById("projects-container");
 
     /* Creates project card */
-    const card = document.createElement('div');
+    const card = document.createElement("div");
     card.id = project.title;
-    card.classList.add('project-card')
-    const imgContainer = document.createElement('div');
-    imgContainer.classList.add('img-container');
-    const infoContainer = document.createElement('div');
-    infoContainer.classList.add('info-container');
+    card.classList.add("project-card");
+    const imgContainer = document.createElement("div");
+    imgContainer.classList.add("img-container");
+    const infoContainer = document.createElement("div");
+    infoContainer.classList.add("info-container");
 
     card.appendChild(imgContainer);
     card.appendChild(infoContainer);
@@ -24,78 +24,82 @@ export const  createProjectCards = () => {
     `;
 
     /* Adds the project title */
-    const projectTitle = document.createElement('h3');
-    projectTitle.classList.add('project-title');
+    const projectTitle = document.createElement("h3");
+    projectTitle.classList.add("project-title");
     projectTitle.innerText = project.title;
 
     /* Adds the technologies used */
-    const technologies = document.createElement('div');
-    technologies.classList.add('technologies');
-    project.technology.forEach(tech => {
-      const techContainer = document.createElement('span');
-      techContainer.classList.add('tech');
+    const technologies = document.createElement("div");
+    technologies.classList.add("technologies");
+    project.technology.forEach((tech) => {
+      const techContainer = document.createElement("span");
+      techContainer.classList.add("tech");
       techContainer.innerText = tech;
 
       switch (tech) {
-        case 'html':
+        case "html":
           techContainer.style.cssText = `
             background-color: #e34c26;
           `;
           break;
-        case 'css':
+        case "css":
           techContainer.style.cssText = `
             background-color: #264de4;
             color: #f1faee;
           `;
           break;
-        case 'CSS Grid':
+        case "CSS Grid":
           techContainer.style.cssText = `
             background-color: #264de4;
             color: #f1faee;
           `;
           break;
-        case 'sass':
+        case "sass":
           techContainer.style.cssText = `background-color: #cd6799`;
           break;
-        case 'javascript':
+        case "javascript":
           techContainer.style.cssText = `background-color: #f0db4f`;
           break;
-        case 'ReactJS':
+        case "ReactJS":
           techContainer.style.cssText = `background-color: #61DBFB`;
           break;
-        case 'Twitter API':
+        case "Twitter API":
           techContainer.style.cssText = `background-color: #00acee`;
           break;
-        case 'ruby':
+        case "ruby":
           techContainer.style.cssText = `
             background-color: #cc0000;
             color: #f1faee;
           `;
           break;
+        case "webpack":
+          techContainer.style.cssText = `background-color: #1C78C0`;
+          break;
+
         default:
           techContainer.style.cssText = `background-color: #a8dadc`;
           break;
       }
 
       technologies.appendChild(techContainer);
-    })
+    });
 
     infoContainer.appendChild(projectTitle);
     infoContainer.appendChild(technologies);
 
     /* Adds the hover buttons */
-    const hoverBtns = document.createElement('div');
-    hoverBtns.classList.add('hover-btns');
-    const liveDemoBtn = document.createElement('a');
+    const hoverBtns = document.createElement("div");
+    hoverBtns.classList.add("hover-btns");
+    const liveDemoBtn = document.createElement("a");
     liveDemoBtn.href = project.projectURL;
-    liveDemoBtn.setAttribute('target', '_blank')
-    liveDemoBtn.classList.add('live-demo-btn');
-    liveDemoBtn.innerText = 'Live Demo';
-    const viewGithubBtn = document.createElement('a');
+    liveDemoBtn.setAttribute("target", "_blank");
+    liveDemoBtn.classList.add("live-demo-btn");
+    liveDemoBtn.innerText = "Live Demo";
+    const viewGithubBtn = document.createElement("a");
     viewGithubBtn.href = project.repoURL;
-    viewGithubBtn.setAttribute('target', '_blank')
-    viewGithubBtn.classList.add('view-github-btn');
-    viewGithubBtn.innerText = 'Source Code';
+    viewGithubBtn.setAttribute("target", "_blank");
+    viewGithubBtn.classList.add("view-github-btn");
+    viewGithubBtn.innerText = "Source Code";
 
     hoverBtns.appendChild(liveDemoBtn);
     hoverBtns.appendChild(viewGithubBtn);
@@ -104,19 +108,19 @@ export const  createProjectCards = () => {
     projectsContainer.appendChild(card);
 
     /* Shows/hides the live demo and view Gihub buttons */
-    card.addEventListener('mouseenter', () => {
+    card.addEventListener("mouseenter", () => {
       card.style.cssText = `
       filter: grayscale(1);
       `;
-      liveDemoBtn.style.cssText = 'display: block';
-      viewGithubBtn.style.cssText = 'display: block';
-    })
-    card.addEventListener('mouseleave', () => {
+      liveDemoBtn.style.cssText = "display: block";
+      viewGithubBtn.style.cssText = "display: block";
+    });
+    card.addEventListener("mouseleave", () => {
       card.style.cssText = `
         filter: grayscale(0);
       `;
-      liveDemoBtn.style.cssText = 'display: none';
-      viewGithubBtn.style.cssText = 'display: none';
-    })
-  })
-}
+      liveDemoBtn.style.cssText = "display: none";
+      viewGithubBtn.style.cssText = "display: none";
+    });
+  });
+};
--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,63 +1,101 @@
 export const projects = [
   {
-    title: 'Restaurant Page',
-    technology: ['html', 'css', 'sass', 'CSS Grid', 'javascript', 'webpack'],
-    projectURL: 'https://madobyte.github.io/restaurant-page/',
-    repoURL: 'https://github.com/Madobyte/restaurant-page',
-    imgURL: './assets/images/restaurant-page.png'
-  },{
-    title: 'Library App',
-    technology: ['html', 'css', 'sass', 'CSS Grid', 'javascript', 'localStorage', 'CRUD'],
-    projectURL: 'https://madobyte.github.io/library-app/',
-    repoURL: 'https://github.com/Madobyte/library-app',
-    imgURL: './assets/images/library-app.png'
-  }, {
-    title: 'Etch-a-Sketch App',
-    technology: ['html', 'css', 'sass', 'CSSGrid', 'javascript', 'html2canvas'],
-    projectURL: 'https://madobyte.github.io/etch-a-sketch/',
-    repoURL: 'https://github.com/Madobyte/etch-a-sketch',
-    imgURL: './assets/images/etch-a-sketch.png'
-  }, {
-    title: 'Rock, Paper, Scissors Game',
-    technology: ['html', 'css', 'CSS Grid', 'Flexbox', 'javascript'],
-    projectURL: 'https://madobyte.github.io/rock-paper-scissors/',
-    repoURL: 'https://github.com/Madobyte/rock-paper-scissors',
-    imgURL: './assets/images/rock-paper-scissors.png'
-  }, {
-    title: 'Drums Project',
-    technology: ['html', 'css', 'CSS Grid', 'javascript'],
-    projectURL: 'https://madobyte.github.io/drums/',
-    repoURL: 'https://github.com/Madobyte/drums',
-    imgURL: './assets/images/drums-project.png'
-  }, {
-    title: 'Bayani Quote Generator',
-    technology: ['ReactJS', 'Twitter API'],
-    projectURL: 'https://madobyte.github.io/Bayani-Quote-Generator/',
-    repoURL: 'https://github.com/Madobyte/Bayani-Quote-Generator',
-    imgURL: './assets/images/bayani-quote-generator.png'
-  }, {
-    title: 'Caesar Cipher using Ruby',
-    technology: ['ruby'],
-    projectURL: '',
-    repoURL: 'https://github.com/Madobyte/caesar_cipher_ruby',
-    imgURL: './assets/images/logo-ruby.png'
-  },{
-    title: 'Google Homepage Mock',
-    technology: ['html', 'css', 'CSS Grid', 'Flexbox'],
-    projectURL: 'https://madobyte.github.io/google-homepage/',
-    repoURL: 'https://github.com/Madobyte/google-homepage',
-    imgURL: './assets/images/google-homepage-mock.png'
-  }, {
-    title: 'FCC Personal Portfolio',
-    technology: ['html', 'css', 'CSS Grid', 'Flexbox'],
-    projectURL: 'https://madobyte.github.io/fcc-personal-portfolio/',
-    repoURL: 'https://github.com/Madobyte/fcc-personal-portfolio',
-    imgURL: './assets/images/fcc-personal-portfolio.png'
-  }, {
-    title: 'Product Landing Page',
-    technology: ['html', 'css', 'Flexbox'],
-    projectURL: 'https://madobyte.github.io/product-landing-page/',
-    repoURL: 'https://github.com/Madobyte/product-landing-page',
-    imgURL: './assets/images/product-landing-page.png'
+    title: "Weather App",
+    technology: [
+      "OpenWeatherMap API",
+      "Geolocation API",
+      "localStorage",
+      "webpack",
+      "sass",
+      "CSS Grid",
+    ],
+    projectURL: "https://madobyte.github.io/weather-app/",
+    repoURL: "https://github.com/Madobyte/weather-app",
+    imgURL: "./assets/images/weather-app.png",
   },
-]
+  {
+    title: "Restaurant Page",
+    technology: ["html", "css", "sass", "CSS Grid", "javascript", "webpack"],
+    projectURL: "https://madobyte.github.io/restaurant-page/",
+    repoURL: "https://github.com/Madobyte/restaurant-page",
+    imgURL: "./assets/images/restaurant-page.png",
+  },
+  {
+    title: "Library App",
+    technology: [
+      "html",
+      "css",
+      "sass",
+      "CSS Grid",
+      "javascript",
+      "localStorage",
+      "CRUD",
+    ],
+    projectURL: "https://madobyte.github.io/library-app/",
+    repoURL: "https://github.com/Madobyte/library-app",
+    imgURL: "./assets/images/library-app.png",
+  },
+  {
+    title: "Etch-a-Sketch App",
+    technology: [
+      "html",
+      "css",
+      "sass",
+      "CSS Grid",
+      "javascript",
+      "html2canvas",
+    ],
+    projectURL: "https://madobyte.github.io/etch-a-sketch/",
+    repoURL: "https://github.com/Madobyte/etch-a-sketch",
+    imgURL: "./assets/images/etch-a-sketch.png",
+  },
+  {
+    title: "Rock, Paper, Scissors Game",
+    technology: ["html", "css", "CSS Grid", "Flexbox", "javascript"],
+    projectURL: "https://madobyte.github.io/rock-paper-scissors/",
+    repoURL: "https://github.com/Madobyte/rock-paper-scissors",
+    imgURL: "./assets/images/rock-paper-scissors.png",
+  },
+  {
+    title: "Drums Project",
+    technology: ["html", "css", "CSS Grid", "javascript"],
+    projectURL: "https://madobyte.github.io/drums/",
+    repoURL: "https://github.com/Madobyte/drums",
+    imgURL: "./assets/images/drums-project.png",
+  },
+  {
+    title: "Bayani Quote Generator",
+    technology: ["ReactJS", "Twitter API"],
+    projectURL: "https://madobyte.github.io/Bayani-Quote-Generator/",
+    repoURL: "https://github.com/Madobyte/Bayani-Quote-Generator",
+    imgURL: "./assets/images/bayani-quote-generator.png",
+  },
+  {
+    title: "Caesar Cipher using Ruby",
+    technology: ["ruby"],
+    projectURL: "",
+    repoURL: "https://github.com/Madobyte/caesar_cipher_ruby",
+    imgURL: "./assets/images/logo-ruby.png",
+  },
+  {
+    title: "Google Homepage Mock",
+    technology: ["html", "css", "CSS Grid", "Flexbox"],
+    projectURL: "https://madobyte.github.io/google-homepage/",
+    repoURL: "https://github.com/Madobyte/google-homepage",
+    imgURL: "./assets/images/google-homepage-mock.png",
+  },
+  {
+    title: "FCC Personal Portfolio",
+    technology: ["html", "css", "CSS Grid", "Flexbox"],
+    projectURL: "https://madobyte.github.io/fcc-personal-portfolio/",
+    repoURL: "https://github.com/Madobyte/fcc-personal-portfolio",
+    imgURL: "./assets/images/fcc-personal-portfolio.png",
+  },
+  {
+    title: "Product Landing Page",
+    technology: ["html", "css", "Flexbox"],
+    projectURL: "https://madobyte.github.io/product-landing-page/",
+    repoURL: "https://github.com/Madobyte/product-landing-page",
+    imgURL: "./assets/images/product-landing-page.png",
+  },
+];
