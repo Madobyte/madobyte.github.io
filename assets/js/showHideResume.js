71cc10352cf20ca06ade5f48bb8d1efa950ecc4e
madobyte <marcbambi@gmail.com>
2021-03-09 17:40:24 +0800

    Added updated resume

--- assets/js/showHideResume.js
+++ assets/js/showHideResume.js
@@ -1,10 +1,13 @@
-import {createModal} from './createModal.js'
+import { createModal } from "./createModal.js";
 
 export const showHideResume = () => {
-  const showHideBtn = document.querySelector('.show-hide-btn');
-  const content = document.createElement('embed');
-  content.src = './assets/pdf/Engr_Marc_Bambalan_Software_Developer.pdf';
-  content.setAttribute('pluginspage', 'http://www.adobe.com/products/acrobat/readstep2.html');
-  
-  showHideBtn.addEventListener('click', () => createModal(content))
-}
+  const showHideBtn = document.querySelector(".show-hide-btn");
+  const content = document.createElement("embed");
+  content.src = "./assets/Engr_Marc_Bambalan_Software_Developer.pdf";
+  content.setAttribute(
+    "pluginspage",
+    "http://www.adobe.com/products/acrobat/readstep2.html"
+  );
+
+  showHideBtn.addEventListener("click", () => createModal(content));
+};
